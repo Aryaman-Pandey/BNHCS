49d9807856e42f10c0e9023281850b1553be178b
Aryaman-Pandey <pandeyavi1808@gmail.com>
2020-08-04 14:11:06 -0700

    Second Commit

--- app/src/main/java/com/aryaman/bnhcs/LoginActivity.java
+++ app/src/main/java/com/aryaman/bnhcs/LoginActivity.java
@@ -91,7 +91,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(LoginActivity.this, "Log In Successful", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            startActivity(new Intent(getApplicationContext(), StudentDashboard.class));
                         }
                         else{
                             Toast.makeText(LoginActivity.this, "Error ! " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
