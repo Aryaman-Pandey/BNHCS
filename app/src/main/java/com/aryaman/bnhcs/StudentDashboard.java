2bc3f2164c058490a65a0b932d15a37a34a2cfb8
Aryaman-Pandey <pandeyavi1808@gmail.com>
2020-08-07 21:43:18 -0700

    Fifth Commit

--- app/src/main/java/com/aryaman/bnhcs/StudentDashboard.java
+++ app/src/main/java/com/aryaman/bnhcs/StudentDashboard.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,10 +16,19 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class StudentDashboard extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
+    ImageView withoutbar;
+    Animation big_anim;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_dashboard);
+
+        withoutbar = (ImageView) findViewById(R.id.withoutbar);
+
+        big_anim = AnimationUtils.loadAnimation(StudentDashboard.this, R.anim.big_anim);
+
+        withoutbar.animate().translationY(50).setDuration(1500).setStartDelay(150);
+        withoutbar.startAnimation(big_anim);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -33,9 +45,8 @@ public class StudentDashboard extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        switch (item.getItemId())
+        if(item.getItemId()==R.id.log_out)
         {
-            case R.id.log_out:
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(StudentDashboard.this, LoginActivity.class));
                 finish();
